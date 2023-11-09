0bcaa0ee50a89068cbe86add4b27b49cb730009d
Ellipsia <80127443+Ellipsia@users.noreply.github.com>
2023-11-09 13:34:18 -0800

    Run Scripts

---
+++ ConfigMgrRightClickScripts/MSICodeRemoval/UninstallMSICode.ps1
@@ -0,0 +1,11 @@
+﻿Param(
+    [Parameter(Mandatory=$True)]
+    [string]$MSICode = "Insert MSI Code (Example: {4282C0BC-3B22-33D4-B72E-62922415DDCB})"
+)
+
+Try {
+    Start-Process "MsiExec.exe" -ArgumentList "/x $MSICode /qn /norestart /l*v C:\Windows\Temp\$MSICode.log"
+}
+Catch {
+    Write-Output "Failed to to remove installed product from system"
+}
---
+++ ConfigMgrRightClickScripts/RemoveFolder/RemoveFolder.ps1
@@ -0,0 +1,11 @@
+﻿Param(
+    [Parameter(Mandatory=$True)]
+    [string]$FolderPath = "C:\_SMSTaskSequence"
+)
+
+Try {
+    Remove-Item -Path $FolderPath -Recurse -Force
+}
+Catch {
+    Write-Output "Failed to remove $FolderPath from remote computer"
+}

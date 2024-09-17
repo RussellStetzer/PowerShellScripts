15d561e18dea58073b421b281e2d510e30834645
Ellipsia <80127443+Ellipsia@users.noreply.github.com>
2024-09-17 09:40:29 -0700

    Update GetDynamicGroupMembership.ps1

--- AD/GetDynamicGroupMembership.ps1
+++ AD/GetDynamicGroupMembership.ps1
@@ -1,1 +1,5 @@
-﻿Get-MgGroup -filter "startswith(displayname, 'device.') and groupTypes/any(c:c eq 'DynamicMembership')" | select DisplayName, Membershiprule
+﻿# Install-Module Microsoft.Graph -Scope AllUsers
+# Install-Module Microsoft.Graph.Beta -Scope AllUsers
+# Connect-MgGraph
+
+Get-MgGroup -filter "startswith(displayname, 'device') and groupTypes/any(c:c eq 'DynamicMembership')" -All | select DisplayName, Membershiprule | Export-Csv C:\Temp\GroupExport.csv

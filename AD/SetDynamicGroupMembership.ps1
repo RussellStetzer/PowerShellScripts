54ad7695da8c3e5b3d9d45d42cb647c7f9520461
Russell Stetzer <614592+RussellStetzer@users.noreply.github.com>
2024-09-18 15:28:48 -0700

    Added verbosity

--- AD/SetDynamicGroupMembership.ps1
+++ AD/SetDynamicGroupMembership.ps1
@@ -3,6 +3,6 @@ foreach ($Group in $Groups)
 {
     if ((get-mggroup -groupid $Group.ID).MembershipRule -notmatch $group.Membershiprule)
     {
-        update-mggroup -GroupID $group.ID -MembershipRule $group.Membershiprule
+        update-mggroup -GroupID $group.ID -MembershipRule $group.Membershiprule | Write-Output
     }
 }

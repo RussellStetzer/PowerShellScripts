c771da388149ae29483ba0e0b68a6188cc93377c
Russell Stetzer <614592+RussellStetzer@users.noreply.github.com>
2024-09-18 15:57:06 -0700

    Update SetDynamicGroupMembership.ps1

--- AD/SetDynamicGroupMembership.ps1
+++ AD/SetDynamicGroupMembership.ps1
@@ -1,8 +1,13 @@
 $Groups = Import-CSV -Path C:\Temp\GroupExport.csv
 foreach ($Group in $Groups)
 {
-    if ((get-mggroup -groupid $Group.ID).MembershipRule -notmatch $group.Membershiprule)
+    if ((get-mggroup -groupid $Group.ID).MembershipRule -match $group.Membershiprule)
+    {
+        Write-Output "No update for "$group.DisplayName
+    }
+    else
     {
         update-mggroup -GroupID $group.ID -MembershipRule $group.Membershiprule | Write-Output
+        Write-Output "Updating " $Group.DisplayName
     }
 }

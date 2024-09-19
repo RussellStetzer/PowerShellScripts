023f7b01a88c55e09e0be43122f2b8926e6f1d81
Russell Stetzer <614592+RussellStetzer@users.noreply.github.com>
2024-09-19 08:56:12 -0700

    Update SetDynamicGroupMembership.ps1
    The check statement was removed due to the difference between the Entra value containing one " while the exported value contained "". I am sure it could be coded to remove and compare those values.

--- AD/SetDynamicGroupMembership.ps1
+++ AD/SetDynamicGroupMembership.ps1
@@ -1,13 +1,6 @@
 $Groups = Import-CSV -Path C:\Temp\GroupExport.csv
 foreach ($Group in $Groups)
-{
-    if ((get-mggroup -groupid $Group.ID).MembershipRule -match $group.Membershiprule)
-    {
-        Write-Output "No update for "$group.DisplayName
-    }
-    else
-    {
+{  
         update-mggroup -GroupID $group.ID -MembershipRule $group.Membershiprule | Write-Output
         Write-Output "Updating " $Group.DisplayName
-    }
 }
